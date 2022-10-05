8ac03fab2d71a9a0c9245ffa0febadd9df7aa847
TomaszSokalski <tsokalski88@gmail.com>
2022-10-05 21:59:02 +0200

    Install depediencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddComponent } from './component/add/add.component';
+import { ViewComponent } from './component/view/view.component';
+import { EditComponent } from './component/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    ViewComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/employeemodel.ts
@@ -0,0 +1,9 @@
+export interface IEmployee{
+    id: number;
+    name: string;
+    mobile: number;
+    gender: string;
+    email: string;
+    position: string;
+    address: string;
+}
